0c15436be0d71831e7ce4d242e8dc86aaa96142e
Dairen Jean <djean032@gmail.com>
2024-05-31 15:46:13 -0400

    read in spectra

---
+++ file_operations.h
@@ -0,0 +1,12 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cstring>
+#include <numeric>
+
+#endif // !FILE_OPERATIONS_H
